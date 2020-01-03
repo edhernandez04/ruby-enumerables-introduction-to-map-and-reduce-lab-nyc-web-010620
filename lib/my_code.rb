e06bfd7aae6986cf8d35e64cb8e3798500544cfa
Eddie Hernandez <mchh.004@gmail.com>
2020-01-03 15:02:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,8 +52,8 @@ end
 def reduce_to_all_true(source)
   index = 0
   while index < source.length
-    return false
     if source[index] === false
+      return false
       index += 1
     end
     return true
